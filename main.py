44c958428adf6272a76104d0371148885720b7b1
migueraoui <mohamedilyesgueraoui@gmail.com>
2024-11-24 19:42:07 +0100

    upload files commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from PenguinLex import penguin_lex
+
+def lookup_term(term):
+    term = term.lower()  
+    if term in penguin_lex:
+        return penguin_lex[term]
+    else:
+        return f"Sorry, the term '{term}' is not in the dictionary."
+
+def add_term(term, definition):
+    term = term.lower() 
+    penguin_lex[term] = definition
+    return f"Term '{term}' has been added to the dictionary."
+
+print(lookup_term('Linux'))  
+print(lookup_term('bash'))  
+
+add_term('Docker', 'A platform for developing, shipping, and running applications in containers.')
+print(lookup_term('docker'))
